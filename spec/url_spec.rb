882c7327894223834cf3b1f0275db8ac89e10013
Martha Berner <marthaberner.mb@gmail.com>
2014-03-27 14:19:05 -0600

    sees a text field w/ explanatory placeholder text and sees a shorten button
    Signed-off-by: Zach Klabunde <zbunde@gmail.com>

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -9,7 +9,8 @@ feature "URL shortener" do
   scenario "user visits homepage" do
 
     visit "/"
-
+    expect(page).to have_field "explanatory placeholder text"
+    expect(page).to have_button "shorten"
   end
 end
 
