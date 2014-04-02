11199a85e6434b024f0a56b3f4bdefceb2be5a08
Martha Berner <marthaberner.mb@gmail.com>
2014-04-02 13:49:27 -0600

    Tweaked form style and syntax
    Signed-off-by: Scott Skender <sjskender@gmail.com>

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -9,14 +9,14 @@ feature "URL shortener" do
   scenario "user visits homepage" do
 
     visit "/"
-    expect(page).to have_field "enter a url to be shortened here"
-    expect(page).to have_button "shorten"
+    expect(page).to have_field 'Enter the URL you would like to "shorten"'
+    expect(page).to have_button "Shorten"
   end
 
   scenario "user submits url to be shortened and old url and new heroku url are displayed" do
     visit "/"
     fill_in "url", with: "wwww.testurl.com"
-    click_on "shorten"
+    click_on "Shorten"
     expect(page).to have_content "www.testurl.com"
     expect(page).to have_content "/1"
   end
